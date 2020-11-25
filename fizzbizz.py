01c81e7a6ebd3d92f05b6f498a1e3d1dde26f3c7
Chuck Liesch <chuck_liesch@wycliffeassociates.org>
2020-11-25 14:52:34 -0600

    t 3:

--- fizzbizz.py
+++ fizzbizz.py
@@ -2,7 +2,7 @@ def fizzbizz():
     for i in range (1-101):
         if i % 3 == 0 |  i % 5 == 0:
             print("Fizzbizz")
-        if i % == 0:
+        if i % 3 == 0:
             print("Fizz")
         if i % 5 == 0:
             print("Bizz")
